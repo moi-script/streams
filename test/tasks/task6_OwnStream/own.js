330db1669d5c230a583da985c88fa88b389d5052
lowkeyCodee <moisesnugal24@gmail.com>
2025-10-29 00:50:30 +0800

    Implemented a readable and writable object mode.

--- test/tasks/task6_OwnStream/own.js
+++ test/tasks/task6_OwnStream/own.js
@@ -2,6 +2,9 @@
 
 import { Readable, Writable } from 'stream';
 import Chance from 'chance';
+import fs, { mkdir } from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
 // -- The reason why we read a stream is to write it to writable
 
 const chance = new Chance();
@@ -49,9 +52,6 @@ const writetable = new Writable({
     }
 })
 
-
-
-
 class TestReadableInstance extends Readable {
     constructor(options) {
         super(options);
@@ -72,20 +72,73 @@ class TestReadableInstance extends Readable {
 }
 
 
-const test = new TestReadableInstance();
+// const test = new TestReadableInstance();
 
-test.print();
-
-
+// test.print();
 
 
 
 
 
 
-// try to create a simple project
-// -- accepting input from terminal -> write to a log file, and watch an update that is maked
-// using terminal readline
+// now lets test readable.pipe();
+
+// readable and writable should be own configuration 
 
 
-// streams.pipe(writetable);
+// the only confusing here is the object mode in writable
+// when a readable pipe into a writable it was not an object but instead just a buffer;
+
+// answer -> both readable and writable should both object mode
+// however upon doing so, it may cause a highWaterMark | threshold
+// const arsReadable = Readable.from([{message1 : 'hello'}, {message2 : 'world'}], {objectMode : true});
+
+
+
+
+const testWritableInstance = new Writable({
+    objectMode : true,
+    write(chunks, encoding, cb) {
+        // const chunksObject = chunks.toString();
+
+        console.log('Chunks recieved value :: ', chunks);
+        const projectFolder = new URL( './test', import.meta.url);
+        const validFilePath = fileURLToPath(projectFolder);
+        
+        // test object mode
+        // console.log("Object mode :: ", chunks.message);
+
+        // create folder and file
+        mkdir(projectFolder).then(() => fs.writeFile(path.join(validFilePath, 'newTXT.txt'), JSON.stringify(chunks))).then(() => cb()).catch(err => console.error(err))
+    }
+});
+
+
+// might be better
+const pipingReadStreamInstance = new Readable({
+    objectMode : true,
+    read() {
+        // always string or buffer
+        this.push(JSON.stringify([{message1 : 'hello'}, {message2 : 'world'}]));
+        this.push(null);
+    }
+})
+
+pipingReadStreamInstance.pipe(testWritableInstance);
+
+
+
+// NEXT TO IMPLEMENT HERE IS DUPLEX AND TRANSFORM 
+
+// -- DUPLEX can be both writable and readable -> meaning we can use both, .write() -> cb, 'data' event -> this.push()
+
+// testWritableInstance.write({message : 'Hi there file'});
+
+// const projectFolder = new URL( './test', import.meta.url);
+//         console.log('Path created :: ', projectFolder);
+
+// mkdir(projectFolder).then(() => fs.writeFile(path.join(fileURLToPath(projectFolder), 'testLol.txt'), 'hello world'));
+
+// console.log('Valid file path :: ', fileURLToPath(projectFolder));
+
+
