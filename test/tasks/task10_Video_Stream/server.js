d5eb5dc2a88814739a2456217aab206c20f9aca5
lowkeyCodee <rizskplay@gmail.com>
2025-12-11 00:17:22 +0800

    Added video extension

--- test/tasks/task10_Video_Stream/server.js
+++ test/tasks/task10_Video_Stream/server.js
@@ -1,13 +1,32 @@
 import express from 'express';
 import cors from 'cors';
 import multer from 'multer';
+import path from 'path';
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
 
-const upload = multer({dest : 'uploads/'})
+// const upload = multer({dest : 'uploads/'})
+
+
+const storage = multer.diskStorage({
+    destination : function(req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename : function (req, file, cb) {
+
+        const ext = path.extname(file.originalname);
+
+        const uniqeName = `${file.fieldname}-${Date.now()}${ext}`
+
+        cb(null, uniqeName);
+    }
+})
+
+
+const upload = multer({storage : storage})
 
 
 app.post('/upload', upload.single('video'), (req, res) => {
