cc40ed672d459274fe8230fd8d269b7294e971b6
lowkeyCodee <rizskplay@gmail.com>
2025-12-10 23:53:43 +0800

    Added multer

--- test/tasks/task10_Video_Stream/server.js
+++ test/tasks/task10_Video_Stream/server.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import cors from 'cors';
+import multer from 'multer';
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
 
-app.post('/upload', (req, res) => {
+const upload = multer({dest : '/uploads'})
+
+
+app.post('/upload', upload.single('video'), (req, res) => {
+
+    console.log('Request :: ', req.file);
     
 })
